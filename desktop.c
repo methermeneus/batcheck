2d18b59e9dda3f543417e316de8bb2b9cf3048c9
Randy Grant <whollycrapproductions@gmail.com>
2017-02-19 20:55:39 -0500

    Formatting cleanup: Tabs looked nice on my computer, but not on GitHub, so now it's all spaces.

--- desktop.c
+++ desktop.c
@@ -31,8 +31,8 @@
 #include <sys/ioctl.h> // ioctl, struct winsize
 #include <unistd.h> // usleep, daemon, pid_t
 #include <string.h> // strerror; Yes, I know strerror isn't threadsafe.
-					// Do you really think this dinky program is going to
-					// implicitly fork threads?
+                    // Do you really think this dinky program is going to
+                    // implicitly fork threads?
 #include <time.h> // struct tm, time_t, localtime, strftime
 
 #define TEMPFILE "/sys/bus/acpi/devices/LNXTHERM:00/thermal_zone/temp"
@@ -49,66 +49,66 @@
 // computer.
 
 int main (int argc, char *argv[], char *env[]) {
-	// If we can't fork the process, there's not much point.
-	pid_t pid = daemon (1,1);
-	if (pid) {
-		fprintf (stderr, "Error, cannot fork process: %s\n", strerror (errno));
-		return (errno);
-	}
-	// Else, lots of variables.
-	struct tm      *currTime;
-	struct winsize xy;
-	       char    timeStr[TIMELEN];
-		   char    tempStr[6];
-	const  char    *tempPath = TEMPFILE;
-	       float   tempC     = 0.0;
-	       float   tempF     = 0.0;
-	       int     x         = 0;
-	       int     textColor = 36;
-	       int     textStyle = 1;
-	       FILE    *infoFile;
-	       time_t  raw;
-	
-	while (1) {
-		time (&raw);
-		currTime = localtime (&raw);
-		strftime (timeStr, TIMELEN, "%a %b %d, %X", currTime);
+    // If we can't fork the process, there's not much point.
+    pid_t pid = daemon (1,1);
+    if (pid) {
+        fprintf (stderr, "Error, cannot fork process: %s\n", strerror (errno));
+        return (errno);
+    }
+    // Else, lots of variables.
+    struct tm      *currTime;
+    struct winsize xy;
+           char    timeStr[TIMELEN];
+           char    tempStr[6];
+    const  char    *tempPath = TEMPFILE;
+           float   tempC     = 0.0;
+           float   tempF     = 0.0;
+           int     x         = 0;
+           int     textColor = 36;
+           int     textStyle = 1;
+           FILE    *infoFile;
+           time_t  raw;
 
-		infoFile = fopen (tempPath, "r");
-		if (infoFile == NULL) {
-			perror ("Error opening temperature file:");
-			return (errno);
-		}
-		if (fgets (tempStr, 6, infoFile) == NULL) {
-			perror ("Error retrieving temperature:");
-			return (errno);
-		}
-		fclose (infoFile);
-		tempC = atof (tempStr) / 1000.0;
-		tempF = (tempC + 32.0) * (9.0 / 5.0);
-		
+    while (1) {
+        time (&raw);
+        currTime = localtime (&raw);
+        strftime (timeStr, TIMELEN, "%a %b %d, %X", currTime);
+
+        infoFile = fopen (tempPath, "r");
+        if (infoFile == NULL) {
+            perror ("Error opening temperature file:");
+            return (errno);
+        }
+        if (fgets (tempStr, 6, infoFile) == NULL) {
+            perror ("Error retrieving temperature:");
+            return (errno);
+        }
+        fclose (infoFile);
+        tempC = atof (tempStr) / 1000.0;
+        tempF = (tempC + 32.0) * (9.0 / 5.0);
+
 #ifndef TERMSIZE
-		fprintf (stderr, "Error, could not retrieve terminal size.\n");
-		return (EXIT_FAILURE);
+        fprintf (stderr, "Error, could not retrieve terminal size.\n");
+        return (EXIT_FAILURE);
 #endif
-		ioctl (1, TERMSIZE, &xy);
-		x = xy.ws_col - TIMELEN;
+        ioctl (1, TERMSIZE, &xy);
+        x = xy.ws_col - TIMELEN;
 
-		printf ("\0337\033[%d;%dm\033[1;%dH[%s]", textStyle, textColor, x, timeStr);
-		// \033 = escape sequence
-		// 7 = save cursor location
-		// [x;ym = text-style x, color y
-		// [y;xH = position (x,y) (in fixed-width character units, from
-		//		origin at top left of screen)
-		x += 4;
-		printf ("\033[2;%dH[%2.1fC] [%2.1F]\033[0m\0338", x, tempC, tempF);
-		// [y;xH again, down one row
-		// [0m = default color and text style
-		// 8 = return to saved cursor location
-		fflush (stdout);
-		usleep (250);
-	}
-	// If we get here, something went wrong
-	fprintf (stderr, "Error, unknown error in info display.\n");
-	return (EXIT_FAILURE);
+        printf ("\0337\033[%d;%dm\033[1;%dH[%s]", textStyle, textColor, x, timeStr);
+        // \033 = escape sequence
+        // 7 = save cursor location
+        // [x;ym = text-style x, color y
+        // [y;xH = position (x,y) (in fixed-width character units, from
+        //        origin at top left of screen)
+        x += 4;
+        printf ("\033[2;%dH[%2.1fC] [%2.1F]\033[0m\0338", x, tempC, tempF);
+        // [y;xH again, down one row
+        // [0m = default color and text style
+        // 8 = return to saved cursor location
+        fflush (stdout);
+        usleep (250);
+    }
+    // If we get here, something went wrong
+    fprintf (stderr, "Error, unknown error in info display.\n");
+    return (EXIT_FAILURE);
 }
--- laptop.c
+++ laptop.c
@@ -32,8 +32,8 @@
 #include <sys/ioctl.h> // ioctl, struct winsize
 #include <unistd.h> // usleep, daemon, pid_t
 #include <string.h> // strerror; Yes, I know strerror isn't threadsafe.
-					// Do you really think this dinky program is going to
-					// implicitly fork threads?
+                    // Do you really think this dinky program is going to
+                    // implicitly fork threads?
 #include <time.h> // struct tm, time_t, localtime, strftime
 
 #define TEMPFILE    "/sys/bus/acpi/devices/LNXTHERM:00/thermal_zone/temp"
@@ -58,123 +58,123 @@
  ********************************************************************/
 
 int main (int argc, char *argv[], char *env[]) {
-	// If we can't fork the process, there's not much point.
-	pid_t pid = daemon (0, 1);
-	if (pid) {
-		fprintf (stderr, "Can't fork process: %s\n", strerror (errno));
-		return (errno);
-	}
-	// Else, lots of variables.
-	struct tm      *currTime;
-	struct winsize xy;
-	       char    timeStr[TIMELEN];
-		   char    tempStr[8]; // @Ambiguous: This means temporary, not temperature.
-	const  char    *tempPath       = TEMPFILE;
-	const  char    *maxChargePath  = TOTALFILE;
-	const  char    *currChargePath = CURRENTFILE;
-	const  char    *acPath = ACFILE;
-		   char    acFlag          = '\0';
-		   char    suffix          = '\0';
-	       float   tempC           = 0.0;
-	       float   tempF           = 0.0;
-	       float   maxCharge       = 0.0;
-	       float   currCharge      = 0.0;
-	       int     x               = 0;
-	       int     percent         = 0;
-	       int     textColor       = 36;
-	       int     textStyle       = 1;
-	       FILE    *infoFile;
-	       time_t  raw;
+    // If we can't fork the process, there's not much point.
+    pid_t pid = daemon (0, 1);
+    if (pid) {
+        fprintf (stderr, "Can't fork process: %s\n", strerror (errno));
+        return (errno);
+    }
+    // Else, lots of variables.
+    struct tm      *currTime;
+    struct winsize xy;
+           char    timeStr[TIMELEN];
+           char    tempStr[8]; // @Ambiguous: This means temporary, not temperature.
+    const  char    *tempPath       = TEMPFILE;
+    const  char    *maxChargePath  = TOTALFILE;
+    const  char    *currChargePath = CURRENTFILE;
+    const  char    *acPath = ACFILE;
+           char    acFlag          = '\0';
+           char    suffix          = '\0';
+           float   tempC           = 0.0;
+           float   tempF           = 0.0;
+           float   maxCharge       = 0.0;
+           float   currCharge      = 0.0;
+           int     x               = 0;
+           int     percent         = 0;
+           int     textColor       = 36;
+           int     textStyle       = 1;
+           FILE    *infoFile;
+           time_t  raw;
 
-	while (1) {
-		time (&raw);
-		currTime = localtime (&raw);
-		strftime (timeStr, 21, "%a %b %d, %X", currTime);
+    while (1) {
+        time (&raw);
+        currTime = localtime (&raw);
+        strftime (timeStr, 21, "%a %b %d, %X", currTime);
 
-		infoFile = fopen (tempPath, "r");
-		if (infoFile == NULL) {
-			perror ("Error opening temperature file:");
-			return (errno);
-		}
-		if (fgets (tempStr, 6, infoFile) == NULL) {
-			perror ("Error retrieving temperature:");
-			return (errno);
-		}
-		fclose (infoFile);
-		tempC = atof (tempStr) / 1000.0;
-		tempF = (tempC + 32.0) * (9.0 / 5.0);
-		
-		// This SHOULD be constant, but I've had crappy laptops where
-		// it wasn't, so we want to know where we are in relation to
-		// the current maximum.
-		infoFile = fopen (maxChargePath, "r");
-		if (infoFile == NULL) {
-			perror ("Error opening max charge file:");
-			return (errno);
-		}
-		if (fgets (tempStr, 8, infoFile) == NULL) {
-			perror ("Error retrieving max charge:");
-			return (errno);
-		}
-		fclose (infoFile);
-		maxCharge = atof (tempStr);
+        infoFile = fopen (tempPath, "r");
+        if (infoFile == NULL) {
+            perror ("Error opening temperature file:");
+            return (errno);
+        }
+        if (fgets (tempStr, 6, infoFile) == NULL) {
+            perror ("Error retrieving temperature:");
+            return (errno);
+        }
+        fclose (infoFile);
+        tempC = atof (tempStr) / 1000.0;
+        tempF = (tempC + 32.0) * (9.0 / 5.0);
 
-		infoFile = fopen (currChargePath, "r");
-		if (infoFile == NULL) {
-			perror ("Error opening current charge file:");
-			return (errno);
-		}
-		if (fgets (tempStr, 8, infoFile) == NULL) {
-			perror ("Error retrieving current charge:");
-			return (errno);
-		}
-		fclose (infoFile);
-		currCharge = atof (tempStr);
-		// In case we somehow have an overcharge:
-		if (currCharge >= maxCharge) percent = 100;
-		else percent = (int) ((currCharge / maxCharge) * 100);
+        // This SHOULD be constant, but I've had crappy laptops where
+        // it wasn't, so we want to know where we are in relation to
+        // the current maximum.
+        infoFile = fopen (maxChargePath, "r");
+        if (infoFile == NULL) {
+            perror ("Error opening max charge file:");
+            return (errno);
+        }
+        if (fgets (tempStr, 8, infoFile) == NULL) {
+            perror ("Error retrieving max charge:");
+            return (errno);
+        }
+        fclose (infoFile);
+        maxCharge = atof (tempStr);
 
-		infoFile = fopen (acPath, "r");
-		if (infoFile == NULL) {
-			perror ("Error opening AC status file:");
-			return (errno);
-		}
-		acFlag = fgetc (infoFile);
-		if (acFlag == EOF) {
-			perror ("Error retrieving AC status:");
-			return (errno);
-		}
-		fclose (infoFile);
-		acFlag -= 48; // make 1 or 0 instead of '1' or '0'
-		// Mnemonic: I looks like a battery, Z like a lightning bolt.
-		suffix = (acFlag ? 'Z' : 'I');
+        infoFile = fopen (currChargePath, "r");
+        if (infoFile == NULL) {
+            perror ("Error opening current charge file:");
+            return (errno);
+        }
+        if (fgets (tempStr, 8, infoFile) == NULL) {
+            perror ("Error retrieving current charge:");
+            return (errno);
+        }
+        fclose (infoFile);
+        currCharge = atof (tempStr);
+        // In case we somehow have an overcharge:
+        if (currCharge >= maxCharge) percent = 100;
+        else percent = (int) ((currCharge / maxCharge) * 100);
+
+        infoFile = fopen (acPath, "r");
+        if (infoFile == NULL) {
+            perror ("Error opening AC status file:");
+            return (errno);
+        }
+        acFlag = fgetc (infoFile);
+        if (acFlag == EOF) {
+            perror ("Error retrieving AC status:");
+            return (errno);
+        }
+        fclose (infoFile);
+        acFlag -= 48; // make 1 or 0 instead of '1' or '0'
+        // Mnemonic: I looks like a battery, Z like a lightning bolt.
+        suffix = (acFlag ? 'Z' : 'I');
 
 #ifndef TERMSIZE
-		fprintf (stderr, "Error, could not retrieve terminal size.\n");
-		return (EXIT_FAILURE);
+        fprintf (stderr, "Error, could not retrieve terminal size.\n");
+        return (EXIT_FAILURE);
 #endif
-		ioctl (1, TERMSIZE, &xy);
-		x = xy.ws_col - TIMELEN;
+        ioctl (1, TERMSIZE, &xy);
+        x = xy.ws_col - TIMELEN;
 
-		printf ("\0337\033[%d;%dm\033[1;%dH[%s]", textStyle, textColor, x, timeStr);
-		// \033 = escape sequence
-		// 7 = save cursor location
-		// [x;ym = text-style x, color y
-		// [y;xH = position (x,y) (in fixed-width character units, from
-		//		origin at top left of screen)
-		// text format 1, color 36 (cyan), first line( from top), xth column (from right), string
-		x += 4; // center smaller text
-		printf ("\033[2;%dH[%2.1fC] [%2.1fF]", x, tempC, tempF);
-		// [y;xH again, down one row
-		x += 4; // smaller text again
-		printf ("\033[3;%dH[%3d%%%c]\033[0m\0338", x, percent, suffix);
-		// [y;xH one more row down
-		// [0m = default color and text style
-		// 8 = return to saved cursor location
-		fflush (stdout);
-		usleep (250);
-	}
-	// If we get here, there's a problem.
-	return (EXIT_FAILURE);
+        printf ("\0337\033[%d;%dm\033[1;%dH[%s]", textStyle, textColor, x, timeStr);
+        // \033 = escape sequence
+        // 7 = save cursor location
+        // [x;ym = text-style x, color y
+        // [y;xH = position (x,y) (in fixed-width character units, from
+        //        origin at top left of screen)
+        // text format 1, color 36 (cyan), first line( from top), xth column (from right), string
+        x += 4; // center smaller text
+        printf ("\033[2;%dH[%2.1fC] [%2.1fF]", x, tempC, tempF);
+        // [y;xH again, down one row
+        x += 4; // smaller text again
+        printf ("\033[3;%dH[%3d%%%c]\033[0m\0338", x, percent, suffix);
+        // [y;xH one more row down
+        // [0m = default color and text style
+        // 8 = return to saved cursor location
+        fflush (stdout);
+        usleep (250);
+    }
+    // If we get here, there's a problem.
+    return (EXIT_FAILURE);
 }
 
